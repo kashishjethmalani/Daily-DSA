f617a9f06fbf93edf44210e3fe16bb0d4b0f6da3
Kashish Jethmalani <kashishjethmalani1203@gmail.com>
2023-06-11 00:41:53 +0530

    Create 01_check if array is sorted.cpp

---
+++ 01_check if array is sorted.cpp
@@ -0,0 +1,22 @@
+
+class Solution
+{
+public:
+    bool check(vector<int> &nums)
+    {
+        int n = nums.size();
+        int cnt = 0;
+        for (int i = 1; i < n; i++)
+        {
+            if (nums[i - 1] > nums[i])
+            {
+                cnt++;
+            }
+        }
+        if (nums[n - 1] > nums[0])
+        {
+            cnt++;
+        }
+        return cnt <= 1;//cnt if 0 or 1, then the array is sorted
+    }
+};
