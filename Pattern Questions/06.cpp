6bd3fd911ac843b614cab308b6d1bc2694b21089
Kashish Jethmalani <kashishjethmalani1203@gmail.com>
2023-08-16 22:44:25 +0530

    Create 06.cpp

---
+++ Pattern Questions/06.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define cy cout << "YES" << endl
+#define cn cout << "NO" << endl
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int k = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = n; j >= i; j--)
+        {
+            cout << k
+                 << " ";
+        k++;
+        }
+        k=1;
+        cout << endl;
+    }
+}
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << setprecision(10) << fixed;
+
+    int testCases = 1;
+    // cin >> testCases;
+    while (testCases--)
+        solve();
+}
