3a02cbfa485c33fbcfc73bcd7a7dc284deb119f2
Kashish Jethmalani <kashishjethmalani1203@gmail.com>
2023-09-06 11:00:04 +0530

    Update 24_copy list with random num.cpp

--- 24_copy list with random num.cpp
+++ 24_copy list with random num.cpp
@@ -4,17 +4,28 @@ https://leetcode.com/problems/copy-list-with-random-pointer/description/
 
 class Solution {
 public:
-    unordered_map<Node*,Node*> m;
     Node* copyRandomList(Node* head) {
-       
-        if(!head)
-            return NULL;
-        Node *tem = new Node(head->val);
-        m[head] = tem;
-        tem->next = copyRandomList(head->next);
-        tem->random = m[head->random];
-        
-        return tem;
+
+        if(!head) return NULL;
+
+        unordered_map<Node*, Node*> m;
+
+        Node*curr=head;
+        while(curr){
+            m[curr] = new Node(curr->val);
+
+            curr=curr->next;
+        }
+
+        curr=head;//for 2nd traversal
+
+        while(curr){
+            m[curr]->next=m[curr->next];
+            m[curr]->random=m[curr->random];
+            curr=curr->next;
+        }
+
+        return m[head];
         
     }
 };
