55982ceddcbcd88f7139a2d7b2a3efd6443b78e9
Kashish Jethmalani <kashishjethmalani1203@gmail.com>
2023-10-04 10:53:43 +0530

    Create 42_counting graphs.cpp

---
+++ 42_counting graphs.cpp
@@ -0,0 +1,10 @@
+
+https://www.codingninjas.com/studio/problems/counting-graphs_8357237?leftPanelTab=1
+
+int countingGraphs(int n)
+{
+    // Write your code here.
+    int x=(n*(n-1))/2;
+    return pow(2,x);
+    
+}
