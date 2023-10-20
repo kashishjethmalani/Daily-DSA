2dd6d8ef7c8e024c11b9c54ae061856fb3c7b271
Kashish Jethmalani <kashishjethmalani1203@gmail.com>
2023-10-20 16:22:50 +0530

    Create 65_flatten nested list iterator_POTD.cpp

---
+++ 65_flatten nested list iterator_POTD.cpp
@@ -0,0 +1,42 @@
+
+https://leetcode.com/problems/flatten-nested-list-iterator/
+
+
+class NestedIterator
+{
+    private:
+    vector<NestedInteger> nestedList;
+    vector<int> flattenedList;
+
+    int currentIndex=0;
+
+    void flatten(vector<NestedInteger> &currentList){
+        for(int i=0;i<currentList.size();i++){
+            if(currentList[i].isInteger()){
+                flattenedList.push_back(currentList[i].getInteger());
+            }
+            else{
+                flatten(currentList[i].getList());
+            }
+        }
+    }
+
+public:
+    NestedIterator(vector<NestedInteger> &nestedList)
+    {
+        this->nestedList=nestedList;
+        flatten(nestedList);
+    }
+
+    int next()
+    {
+        int number=flattenedList[currentIndex];
+        currentIndex++;
+        return number;
+    }
+
+    bool hasNext()
+    {
+        return currentIndex<flattenedList.size();
+    }
+};
