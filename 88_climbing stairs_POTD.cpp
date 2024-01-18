1692ac7c1448b5c236c61a9bd16e31549d64ecdb
Kashish Jethmalani <kashishjethmalani1203@gmail.com>
2024-01-18 13:52:55 +0530

    Create 88_climbing stairs_POTD.cpp

---
+++ 88_climbing stairs_POTD.cpp
@@ -0,0 +1,23 @@
+
+https://leetcode.com/problems/climbing-stairs/
+
+class Solution {
+public:
+
+    int solve(int i, vector<int> &dp){
+        if(i<0) return 0;
+        if(i==0) return 1;
+
+        if(dp[i]!=-1) {return dp[i];}
+
+        dp[i]=solve(i-1, dp) + solve(i-2, dp);
+        return dp[i];
+
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return solve(n, dp);
+         
+        
+    }
+};
